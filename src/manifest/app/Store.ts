8dc930a4337e12f8856dfeb137786231131c2cf7
Nikola Malovic <nikola.malovic@outreach.io>
2021-08-11 08:29:01 +0200

    Adding some comments on Store

--- src/manifest/app/Store.ts
+++ src/manifest/app/Store.ts
@@ -8,7 +8,18 @@
  * @enum {number}
  */
 export enum Store {
+  /**
+   * This is a private store where only invited OR users have access.
+   * Main purpose is to enable developers working on an application
+   */
   PRIVATE = 'private',
+  /**
+   * This is a internal store accessible by anyone in a organization
+   * owning this store.
+   */
   INTERNAL = 'internal',
+  /**
+   * This is public store accessible by any Outreach user.
+   */
   PUBLIC = 'public',
 }
