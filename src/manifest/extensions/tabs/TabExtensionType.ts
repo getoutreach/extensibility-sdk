96907ebe0cd7ac9c1bfb6e92d9a9898724e940a0
Nikola Malovic <nikola.malovic@outreach.io>
2021-10-18 10:22:20 +0200

    Correcting tab ext types

--- src/manifest/extensions/tabs/TabExtensionType.ts
+++ src/manifest/extensions/tabs/TabExtensionType.ts
@@ -12,22 +12,41 @@ export enum TabExtensionType {
   ACCOUNT = 'tab-account',
 
   /**
+   * Sidebar shown on the right side of the account page
+   */
+  ACCOUNT_SIDEBAR = 'tab-account-sidebar',
+
+  /**
+   * Sidebar action button shown in the account page tab right sidebar
+   */
+  ACCOUNT_ACTION = 'tab-account-action',
+
+  /**
    * Opportunity details tab extension
    */
   OPPORTUNITY = 'tab-opportunity',
 
+  /**
+   * Sidebar shown on the right side of the opportunity page
+   */
+  OPPORTUNITY_SIDEBAR = 'tab-opportunity-sidebar',
+
+  /**
+   * Sidebar action button shown in the opportunity page tab right sidebar
+   */
+  OPPORTUNITY_ACTION = 'tab-opportunity-action',
   /**
    * Prospect details tab extension
    */
   PROSPECT = 'tab-prospect',
 
   /**
-   * Sidebar shown on the right side of the tabs
+   * Sidebar shown on the right side of the prospect page
    */
-  SIDEBAR = 'tab-sidebar',
+  PROSPECT_SIDEBAR = 'tab-prospect-sidebar',
 
   /**
-   * Sidebar action button shown in the tab right sidebar
+   * Sidebar action button shown in the prospect page tab right sidebar
    */
   PROSPECT_ACTION = 'tab-prospect-action',
 }
