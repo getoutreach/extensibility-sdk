6b595e48bdced0aa87534a7ce554d7c7bc7b8126
sheff146 <alex.shevchenko@outreach.io>
2022-05-20 15:25:21 +0200

    Update tests

--- tests/validator.test.ts
+++ tests/validator.test.ts
@@ -71,10 +71,11 @@ describe('manifest tests', () => {
     test('applicationId should be defined', () => {
       const manifest = getNewValidApplicationManifest();
 
+      delete (manifest.api as any).client;
       delete (manifest.api as any).applicationId;
       var issues = validate(manifest);
-      expect(issues.length).toBe(1);
-      expect(issues[0]).toBe('Manifest Api section needs to have applicationId value.');
+      expect(issues.length).toBe(2);
+      expect(issues[1]).toBe('Manifest Api section needs to have applicationId value.');
     });
 
     test('client should be defined', () => {
@@ -97,11 +98,12 @@ describe('manifest tests', () => {
 
     test('redirectUri should be valid URL', () => {
       const manifest: Application = getNewValidApplicationManifest();
+      delete (manifest.api as any).redirectUris;
       manifest.api!.redirectUri = 'bananas';
 
       var issues = validate(manifest);
-      expect(issues.length).toBe(1);
-      expect(issues[0]).toBe('Manifest Api section needs to have a valid redirect url. Value: bananas');
+      expect(issues.length).toBe(2);
+      expect(issues[1]).toBe('Manifest Api section needs to have a valid redirect url. Value: bananas');
     });
 
     test('redirectUris should be valid URLs', () => {
