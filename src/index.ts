6a604c5060128c29b3883be5a4fe2c7b3d05561a
Nikola Malovic <nikola.malovic@outreach.io>
2022-10-01 11:38:50 +0200

    oree-870 adding type guard needed for client  fix

--- src/index.ts
+++ src/index.ts
@@ -91,7 +91,7 @@ export { RuntimeContext } from './sdk/RuntimeContext';
 export { Theme } from './sdk/Theme';
 export { validate } from './sdk/Validator';
 
-export { utils } from './utils';
+export { hasAppIdProperty, utils } from './utils';
 
 export { Application } from './manifest/Application';
 export { ManifestApi } from './manifest/ManifestApi';
