e5a8acfc1196d86208408f621dcdf90f35487c48
Petr Šmejkal <32209097+PeSme@users.noreply.github.com>
2026-03-20 15:50:06 +0100

    feat: introduce tile groups [ADS-2230] (#168)

---
+++ src/manifest/extensions/tiles/TileGroup.ts
@@ -0,0 +1,132 @@
+/* eslint-disable no-unused-vars */
+/**
+ * List of supported groups for grouping tiles in the Add Tile panel.
+ * @export
+ * @enum {number}
+ */
+export enum TileGroup {
+  /**
+   * Account based marketing extension group
+   */
+  ACCOUNT_BASED_MARKETING = 'ACCOUNT_BASED_MARKETING',
+
+  /**
+   * Account plan extension group
+   */
+  ACCOUNT_PLAN = 'ACCOUNT_PLAN',
+
+  /**
+   * Chat extension group
+   */
+  CHAT = 'CHAT',
+
+  /**
+   * CRM extension group
+   */
+  CRM = 'CRM',
+
+  /**
+   * Direct mail extension group
+   */
+  DIRECT_MAIL = 'DIRECT_MAIL',
+
+  /**
+   * Inbox extension group
+   */
+  INBOX = 'INBOX',
+
+  /**
+   * Integration platform extension group
+   */
+  INTEGRATION_PLATFORM = 'INTEGRATION_PLATFORM',
+
+  /**
+   * Marketing extension group
+   */
+  MARKETING = 'MARKETING',
+
+  /**
+   * Privacy and security extension group
+   */
+  PRIVACY_SECURITY = 'PRIVACY_SECURITY',
+
+  /**
+   * Sales assets management extension group
+   */
+  SALES_ASSETS_MANAGEMENT = 'SALES_ASSETS_MANAGEMENT',
+
+  /**
+   * Sales intelligence data extension group
+   */
+  SALES_INTELLIGENCE_DATA = 'SALES_INTELLIGENCE_DATA',
+
+  /**
+   * Sales productivity extension group
+   */
+  SALES_PRODUCTIVITY = 'SALES_PRODUCTIVITY',
+
+  /**
+   * Video extension group
+   */
+  VIDEO = 'VIDEO',
+
+  /**
+   * Voice extension group
+   */
+  VOICE = 'VOICE',
+
+  /**
+   * Admin Dashboard extension group
+   */
+  ADMIN = 'ADMIN',
+
+  /**
+   * Meetings extension group
+   */
+  MEETINGS = 'MEETINGS',
+
+  /**
+   * Data warehouse extension group
+   */
+  DATA_WAREHOUSE = 'DATA_WAREHOUSE',
+
+  /**
+   * Reports Dashboard extension group
+   */
+  REPORTS = 'REPORTS',
+
+  /**
+   * Records extension group
+   */
+  RECORDS = 'RECORDS',
+
+  /**
+   * Grouping and navigation group
+   */
+  GROUPING_AND_NAVIGATION = 'GROUPING_AND_NAVIGATION',
+
+  /**
+   * Agents group
+   */
+  AGENTS = 'AGENTS',
+
+  /**
+   * Execution group
+   */
+  EXECUTION = 'EXECUTION',
+
+  /**
+   * Performance group
+   */
+  PERFORMANCE = 'PERFORMANCE',
+
+  /**
+   * Fields group
+   */
+  FIELDS = 'FIELDS',
+
+  /**
+   * Health and activity group
+   */
+  HEALTH_AND_ACTIVITY = 'HEALTH_AND_ACTIVITY',
+}
